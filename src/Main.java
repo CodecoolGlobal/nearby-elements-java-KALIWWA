4586a08b280763a52c539f0df089709966d755e2
Michal Kalinowski <subaru.kali@gmail.com>
2019-04-23 14:40:19 +0200

    new results added

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,13 @@ public class Main {
         Result result1 = elem.nearby(0,2,2);
         Result result2 = elem.nearby(1, 0, 1);
         Result result3 = elem.nearby(1, 3, 5);
+        Result result4 = elem.nearby(1, 3, -1);
+        Result result5 = elem.nearby(1, 3, 0);
 
         System.out.println(result1);
         System.out.println(result2);
         System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
     }
 }
