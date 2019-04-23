b62530c3bbf613fd8345a5d26d4b1d9a1fa99430
Michal Kalinowski <subaru.kali@gmail.com>
2019-04-23 14:14:24 +0200

    refactor

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,6 @@ public class Main {
         Result result2 = elem.nearby(1, 0, 1);
         Result result3 = elem.nearby(1, 3, 5);
 
-        result1.toString();
-        result2.toString();
-        result3.toString();
-
         System.out.println(result1);
         System.out.println(result2);
         System.out.println(result3);
